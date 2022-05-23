16086d771d70e0185408ea8815030f8479c5a642
AlexGnSoft <gnuskin.ae@gmail.com>
2022-05-23 16:44:40 +0100

    new updated

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import pageobjects.openPage.OpenPage;
 
 public class BaseTest {
-    protected WebDriver driver = BaseTestConfiguration.createDriver();
+    protected static WebDriver driver = BaseTestConfiguration.createDriver();
     protected PropertiesFile propertiesFile = new PropertiesFile();
     protected OpenPage openPage = new OpenPage(driver);
     protected BaseTestConfiguration baseTestConfiguration = new BaseTestConfiguration(driver);
