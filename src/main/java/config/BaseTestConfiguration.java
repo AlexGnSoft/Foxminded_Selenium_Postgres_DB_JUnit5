5a7b213662343703b2a11a5ee7069d6cb7f2b201
Hnuskyn Oleksandr <gnuskin.ae@gmail.com>
2022-05-24 13:56:32 +0000

    Update BaseTestConfiguration.java

--- src/main/java/config/BaseTestConfiguration.java
+++ src/main/java/config/BaseTestConfiguration.java
@@ -1,1 +1,44 @@
+package config;
 
+import helpfiles.PropertiesFile;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+
+public class BaseTestConfiguration {
+    protected static PropertiesFile propertiesFile;
+    protected static WebDriver driver;
+
+    @BeforeEach
+    public void createDriver(){
+        propertiesFile = new PropertiesFile();
+        switch(propertiesFile.getBrowser()){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", propertiesFile.getDriverPathChrome());
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", propertiesFile.getDriverPathFireFox());
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("Browser name specified in Config class is not equal to : " + propertiesFile.getBrowser());
+        }
+        assert driver != null;
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(propertiesFile.getImplicitWait()));
+    }
+
+
+    public void openBrowser(){
+        driver.get(propertiesFile.getApplicationUrl());
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
