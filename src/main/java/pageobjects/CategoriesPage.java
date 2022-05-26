13054e5e231528ea273f201e4ced2cbd2a5babcf
AlexGnSoft <gnuskin.ae@gmail.com>
2022-05-26 17:45:16 +0100

    Page locators_updates

--- src/main/java/pageobjects/CategoriesPage.java
+++ src/main/java/pageobjects/CategoriesPage.java
@@ -6,10 +6,9 @@ import org.openqa.selenium.By;
 public class CategoriesPage extends BaseTestConfiguration {
     private static final By categoriesPage = By.xpath("//div[@class='container-fluid']");
     private static final By newCategoryBtn = By.xpath("//button[@id='new-category-btn']");
-    private static final By firstEditBtn = By.xpath("//tbody/tr[2]/td[3]/a[1]/i[1]");
-    private static final By secondEditBtn = By.xpath("//tbody/tr[3]/td[3]/a[1]/i[1]");
-    private static final By deleteBtn = By.xpath("//tbody/tr[3]/td[4]/a[1]");
-    private static final By fistCategoryInTheList = By.xpath("//tbody/tr[2]/td[1]/a[1]");
+    private static final By editBtnList = By.xpath("//a[@id='category-stage-edit-btn']");
+    private static final By deleteBtnList = By.xpath("//a[@id='category-delete-btn']");
+    private static final By colorLabelList = By.xpath("//span[@class='label label-lg']");
 
 
 
