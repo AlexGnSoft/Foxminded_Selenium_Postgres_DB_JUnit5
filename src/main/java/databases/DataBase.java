e8ae95a1401c7ccf6b78031debba5f335a7658a3
AlexGnSoft <gnuskin.ae@gmail.com>
2022-06-27 22:58:13 +0100

    getListOfValues is done

--- src/main/java/databases/DataBase.java
+++ src/main/java/databases/DataBase.java
@@ -29,7 +29,7 @@ public class DataBase {
     /**
      * Method is used to get a single value from the database
      */
-    public String getStringValue(String query, int columnIndex) throws SQLException {
+    public String getStringValue(String query, String tableName, int columnIndex) throws SQLException {
         String resultString = null;
         Connection connection = null;
 
@@ -38,11 +38,13 @@ public class DataBase {
             PropertiesFile propertiesFile = new PropertiesFile();
             connection = getConnection(propertiesFile.getDataBaseLogin(), propertiesFile.getDataBasePassword());
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(query);
+            ResultSet rs = statement.executeQuery(query + tableName);
 
             if (rs.next()) {
                 resultString = rs.getString(columnIndex);
             }
+
+            System.out.println(resultString);
 
         } catch (Exception e) {
             System.out.println("Close connection");
@@ -60,67 +62,53 @@ public class DataBase {
      */
     public ArrayList<String> getListOfValues(String query) throws SQLException {
         ArrayList<String> columnFields = new ArrayList<>();
-        String resultString = null;
         Connection connection = null;
 
-        try {
-            //open connection here
-            PropertiesFile propertiesFile = new PropertiesFile();
-            connection = getConnection(propertiesFile.getDataBaseLogin(), propertiesFile.getDataBasePassword());
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            ResultSetMetaData rsmd = resultSet.getMetaData();
-            int columnsNumber = rsmd.getColumnCount();
-            while (resultSet.next()) {
-                for (int i = 1; i <= columnsNumber; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = resultSet.getString(i);
+        //open connection here
+        PropertiesFile propertiesFile = new PropertiesFile();
+        connection = getConnection(propertiesFile.getDataBaseLogin(), propertiesFile.getDataBasePassword());
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+        // get column count
+        int columnCount = rsmd.getColumnCount();
+        // print column Label
+        for (int i = 1; i <=columnCount ; ++i) {
+            System.out.print(rsmd.getColumnLabel(i) + "\t\t\t\t");
+        }
+        System.out.println();
+        System.out.println();
 
-                    System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
-
-                    String str = rsmd.getColumnName(i) + ": " + columnValue;
-                    columnFields.add(str);
-                }
-                System.out.println("");
+        // print column Values
+        while (resultSet.next()) {
+            for (int i = 1; i <= columnCount; ++i) {
+                String columnValue = resultSet.getString(i);
+                System.out.print(columnValue + "\t\t\t\t");
+                String str = rsmd.getColumnName(i) + ": " + columnValue;
+                columnFields.add(str);
             }
+            System.out.println();
 
-        } catch (Exception e) {
-            System.out.println("Close connection");
-        } finally {
             if (!connection.isClosed()) {
                 connection.close();
-                System.out.println();
             }
         }
         return columnFields;
     }
 
-
-        /**
-         * Method is used to get a String
-         */
-//        public static String getString () {
-//
-//        }
-
-        /**
-         * Method is used to get a Map
-         */
+    /**
+     * Method is used to get a Map
+     */
 //        public static HashMap<String, String> getMap () {
 //
 //        }
 
-        /**
-         * Method is used to get a List
-         */
-//        public static List getList () {
-//
-//        }
-        public static void main (String[]args) throws SQLException {
-            DataBase db = new DataBase();
-            PropertiesFile propertiesFile = new PropertiesFile();
-            db.getListOfValues("select ticket.company_ticket_id as id from ticket left join category on ticket.id=category_id where creation_timestamp >='19.04.2018' and done_deadline <='03.05.2018'");
-        }
+
+    public static void main (String[]args) throws SQLException {
+        DataBase db = new DataBase();
+        PropertiesFile propertiesFile = new PropertiesFile();
+        db.getListOfValues("select * from ticket");
     }
+}
 
 
