f65ea73e0beae0da3c00212f86ed3c751394d999
AlexGnSoft <gnuskin.ae@gmail.com>
2022-07-11 19:00:44 +0100

    TestSuite is ready + Parametrized Locator for ContactsNewContactPage_Param_Locator.class !!!

--- src/main/java/pageobjects/ContactsNewContactPage_Param_Locator.java
+++ src/main/java/pageobjects/ContactsNewContactPage_Param_Locator.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.List;
 
 
